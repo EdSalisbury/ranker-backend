115224536575d8ac7b4dfb20ee7b4b0337e83f80
Ed Salisbury <ed.salisbury@gmail.com>
2021-03-21 12:55:25 -0600

    Added basic functionality - needs cleanup but it works.

--- app.js
+++ app.js
@@ -3,39 +3,62 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const router = express.Router()
+const mongoose = require('mongoose')
+const restify = require('express-restify-mongoose');
+const methodOverride = require('method-override')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+mongoose.connect('mongodb://localhost:27017/database')
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+restify.serve(router, mongoose.model('items', new mongoose.Schema({
+  title: { type: String, required: true },
+  image: { type: Buffer },
+  index: { type: Number },
+  rating: { type: Number }
+})))
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(router)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.listen(3000, () => {
+//   console.log('Express server listening on port 3000')
+// })
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
